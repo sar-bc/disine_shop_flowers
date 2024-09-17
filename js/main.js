22e8a960288bc28e6da379bb02cb7f3582175685
Виктор <sar-bc@mail.ru>
2024-09-18 03:54:45 +0400

    version_8

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ window.addEventListener('scroll', function () {
 });
 
 $(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+    });
+
+    $('#top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
+
     $(".owl-carousel-full").owlCarousel({
         margin: 20,
         responsive: {
